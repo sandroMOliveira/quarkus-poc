eb4d092bb3cba2343fc62abadf34c2738dd41051
sandrooliveira-hotmart <sandro.oliveira@hotmart.com>
2021-07-09 09:43:59 -0300

    fix tests

--- src/test/kotlin/org/poc/ApiControllerTest.kt
+++ src/test/kotlin/org/poc/ApiControllerTest.kt
@@ -2,19 +2,21 @@ package org.poc
 
 import io.quarkus.test.junit.QuarkusTest
 import io.restassured.RestAssured.given
-import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.CoreMatchers.containsString
 import org.junit.jupiter.api.Test
 
 @QuarkusTest
 class ApiControllerTest {
 
     @Test
-    fun testHelloEndpoint() {
+    fun `get images of the day from nasa`() {
         given()
-          .`when`().get("/hello")
+          .`when`().get("/rest/v1/nasa/apod/5")
           .then()
              .statusCode(200)
-             .body(`is`("Hello RESTEasy"))
+            .body(containsString("date"))
+            .body(containsString("explanation"))
+            .body(containsString("url"))
     }
 
 }
